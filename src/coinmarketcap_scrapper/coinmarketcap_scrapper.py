5522fc98215e0d5245d65db5efbb8f1e497a0217
ran <ran-n@tutanota.com>
2022-01-02 12:56:17 +0100

    + obxecto base & getters & setters & tests

--- src/coinmarketcap_scrapper/coinmarketcap_scrapper.py
+++ src/coinmarketcap_scrapper/coinmarketcap_scrapper.py
@@ -3,7 +3,42 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2022/01/01 20:23:55.455964
-#+ Editado:	2022/01/01 20:23:55.455964
+#+ Editado:	2022/01/02 12:48:37.224221
 # ------------------------------------------------------------------------------
+from typing import Optional, List, Union
+# ------------------------------------------------------------------------------
+class CoinMarketCap:
+    # atributos de clase
+    __pax: int = 1
+    __moeda: str = 'eur'
+    __url: str = f'https://coinmarketcap.com/?page={__pax}&currency={__moeda}'
+
+    # Constructor --------------------------------------------------------------
+    def __init__(self, moeda: Optional[str] = 'eur') -> None:
+        # variables da instancia
+        self.__pax = self.__pax
+        self.__moeda = moeda
+        self.__url = self.__url
+    # --------------------------------------------------------------------------
+
+    # Getters ------------------------------------------------------------------
+    def get_pax(self) -> int:
+        return self.__pax
+
+    def get_moeda(self) -> str:
+        return self.__moeda
+
+    def get_url(self) -> str:
+        return self.__url
+    # --------------------------------------------------------------------------
+
+    # Setters ------------------------------------------------------------------
+    def set_pax(self, nova_pax) -> None:
+        self.__pax = nova_pax
+
+    def set_moeda(self, nova_moeda) -> None:
+        self.__moeda = nova_moeda
+    # --------------------------------------------------------------------------
+
 # ------------------------------------------------------------------------------
 
--- tests/coinmarketcap_scrapper_test.py
+++ tests/coinmarketcap_scrapper_test.py
@@ -3,6 +3,72 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2022/01/02 12:34:15.530170
-#+ Editado:	2022/01/02 12:34:15.530170
+#+ Editado:	2022/01/02 12:55:20.559068
+# ------------------------------------------------------------------------------
+import unittest
+
+from src.coinmarketcap_scrapper.coinmarketcap_scrapper import CoinMarketCap
+# ------------------------------------------------------------------------------
+class TestCoinMarketCap_scrapper(unittest.TestCase):
+
+    @staticmethod
+    def get_url(pax=1, moeda='eur'):
+        return f'https://coinmarketcap.com/?page={pax}&currency={moeda}'
+
+    # Getters ------------------------------------------------------------------
+
+    def test_get_pax(self):
+        """
+        Uso normal.
+        """
+
+        cmc = CoinMarketCap()
+
+        self.assertEqual(cmc.get_pax(), 1)
+
+    def test_get_moeda(self):
+        """
+        Uso normal.
+        """
+
+        cmc = CoinMarketCap()
+        self.assertEqual(cmc.get_moeda(), 'eur')
+
+    def test_get_url(self):
+        """
+        Uso normal.
+        """
+
+        cmc = CoinMarketCap()
+        self.assertEqual(cmc.get_url(), self.get_url())
+
+    # --------------------------------------------------------------------------
+
+    # Setters ------------------------------------------------------------------
+
+    def test_set_pax(self):
+        """
+        Uso normal.
+        """
+
+        cmc = CoinMarketCap()
+
+        self.assertEqual(cmc.get_pax(), 1)
+        cmc.set_pax(5)
+        self.assertEqual(cmc.get_pax(), 5)
+
+    def test_set_moeda(self):
+        """
+        Uso normal.
+        """
+
+        cmc = CoinMarketCap()
+
+        self.assertEqual(cmc.get_moeda(), 'eur')
+        cmc.set_moeda('usd')
+        self.assertEqual(cmc.get_moeda(), 'usd')
+
+    # --------------------------------------------------------------------------
+
 # ------------------------------------------------------------------------------
 
