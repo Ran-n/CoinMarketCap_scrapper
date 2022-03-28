27f86e1dc573306f4cbc28e52dd51eeaa7a4888d
Ran-n <ran-n@tutanota.com>
2022-03-28 22:52:37 +0200

    +entradas

--- media/db/get_ligs.py
+++ media/db/get_ligs.py
@@ -323,10 +323,12 @@ def scraping() -> None:
         #scrape_inicio(cur, info_db_ini, r)
         scrapi_inicio(cur, info_db_ini, r)
 
+        """
         scrape(cur, info_db_ini, 'gan_per', r)
         scrape(cur, info_db_ini, 'trending', r)
         scrape(cur, info_db_ini, '+visit', r)
         scrape(cur, info_db_ini, 'novos', r)
+        """
 
 
     except KeyboardInterrupt:
